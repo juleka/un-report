46f0e488679ca3e6ca90273307e7ea2553568e6f
juleka <jule@hrdag.org>
2021-01-12 11:41:02 -0500

    adding r for data analysis lesson

---
+++ code/un_data_analysis.R
@@ -0,0 +1,33 @@
+library(tidyverse)
+library(tidyr)
+
+gapminder_data <- read_csv("data/gapminder_data.csv")
+
+gapminder_data %>% 
+  filter(year==1952) %>% 
+  summarize(averageGDP_percap = mean(gdpPercap), 
+            first_year = min(year))
+
+#group_by
+gapminder_data %>% 
+  group_by(continent) %>% 
+  summarize(averageLifeExp = mean(lifeExp))
+
+#arrange
+
+# mutate - add more columns to your dataset
+gapminder_data %>%
+  mutate(popMil = pop/1000000)
+
+# select
+gapminder_data %>% 
+  select(-continent)
+
+# long vs. wide data format: pivot_longer/pivot_wider
+gapminder_data %>%
+  select(country, continent, year, lifeExp) %>% 
+  pivot_wider(names_from = year, values_from = lifeExp)
+## for some reason, i cannot update tidyverse 
+##   to get function pivot_wider
+
+
